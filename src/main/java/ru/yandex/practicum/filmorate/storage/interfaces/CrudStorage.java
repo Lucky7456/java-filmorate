53f454f99cdf8bc6c5ec9a03cefd47485ac77c1c
Lucky7456 <amir.abdrahmanov@yandex.ru>
2024-10-06 09:54:22 +0500

    fix: remove redundant method

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/CrudStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/CrudStorage.java
@@ -1,11 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.interfaces;
 
-import java.util.Optional;
-
 public interface CrudStorage<T> {
     long create(T entity);
-    
-    Optional<T> findOneById(long id);
     
     boolean update(Object... params);
     
